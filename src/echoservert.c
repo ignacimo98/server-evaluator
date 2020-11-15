9042ddbaf4ac6fe949562fa8ec3ebb645c5a1252
h4koo <georgeaz56@gmail.com>
2020-11-14 20:01:18 -0600

    modulo sobel, servidor compila en Linux

--- src/echoservert.c
+++ src/echoservert.c
@@ -3,35 +3,39 @@
  */
 
 #include "nethelp.h"
+#include <pthread.h>
 
 void echo(int connfd);
 void *thread(void *vargp);
 
-int main(int argc, char **argv) 
+int main(int argc, char **argv)
 {
-    int listenfd, *connfdp, port, clientlen=sizeof(struct sockaddr_in);
+    int listenfd, *connfdp, port;
+    socklen_t clientlen = sizeof(struct sockaddr_in);
     struct sockaddr_in clientaddr;
-    pthread_t tid; 
+    pthread_t tid;
 
-    if (argc != 2) {
-	fprintf(stderr, "usage: %s <port>\n", argv[0]);
-	exit(0);
+    if (argc != 2)
+    {
+        fprintf(stderr, "usage: %s <port>\n", argv[0]);
+        exit(0);
     }
     port = atoi(argv[1]);
 
     listenfd = open_listenfd(port);
-    while (1) {
-	connfdp = malloc(sizeof(int));
-	*connfdp = accept(listenfd, (struct sockaddr*)&clientaddr, &clientlen);
-	pthread_create(&tid, NULL, thread, connfdp);
+    while (1)
+    {
+        connfdp = malloc(sizeof(int));
+        *connfdp = accept(listenfd, (struct sockaddr *)&clientaddr, &clientlen);
+        pthread_create(&tid, NULL, thread, connfdp);
     }
 }
 
 /* thread routine */
-void * thread(void * vargp) 
-{  
+void *thread(void *vargp)
+{
     int connfd = *((int *)vargp);
-    pthread_detach(pthread_self()); 
+    pthread_detach(pthread_self());
     free(vargp);
     echo(connfd);
     close(connfd);
@@ -41,13 +45,14 @@ void * thread(void * vargp)
 /*
  * echo - read and echo text lines until client closes connection
  */
-void echo(int connfd) 
+void echo(int connfd)
 {
-    size_t n; 
-    char buf[MAXLINE]; 
+    size_t n;
+    char buf[MAXLINE];
 
-    while((n = readline(connfd, buf, MAXLINE)) != 0) {
-	printf("server received %d bytes\n", n);
-	write(connfd, buf, n);
+    while ((n = readline(connfd, buf, MAXLINE)) != 0)
+    {
+        printf("server received %ld bytes\n", n);
+        write(connfd, buf, n);
     }
 }
