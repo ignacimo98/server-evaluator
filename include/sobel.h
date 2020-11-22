e49bb2d607d9151bcd90d0a0b06719c78e7a6cd7
h4koo <georgeaz56@gmail.com>
2020-11-22 00:14:54 -0600

    blah

--- include/sobel.h
+++ include/sobel.h
@@ -1,6 +1,10 @@
 #ifndef _SOBEL_H
 #define _SOBEL_H
 
-void apply_filter(char* filename);
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+void apply_filter(char *filename);
 
 #endif // _SOBEL_H
