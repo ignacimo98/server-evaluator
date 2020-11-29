e925036853af9b9ca0fc45ec51ee02843c7ea9cf
Jung <bakkim05@gmail.com>
2020-11-28 22:08:51 -0800

    updates

--- visualizador/visual_test.py
+++ visualizador/visual_test.py
@@ -6,16 +6,60 @@ import math
 import matplotlib.pyplot as plt
 
 def graph_data():
-    num_subplots = len(datman.M_Thread_Array)
+    #TOTAL TIME
+    num_threads_x_time = plt.figure(figsize=(16,16))
+    num_threads_x_time.suptitle("Total Time vs Number of Images")
 
-    t = np.linspace(0,2*math.pi,400)
-    a = np.sin(t)
-    b = np.cos(t)
-    c = a + b
+    temp = len(datman.M_Thread_Array)
+    graph_rows = math.ceil(temp/2)
+    cont = 1
 
-    plt.plot(t, a, t, b, t, c)
+    for i in datman.M_Thread_Array:
+        num_threads_x_time.add_subplot(graph_rows,2, cont).title.set_text("Threads: " + str(i.id))
+        num_images = [cycle * int(i.id) for cycle in i.measurements[0].cycle]
+
+        for m in i.measurements:
+            plt.plot(num_images, m.time, label = name_changer(m.file_name),marker = "o")
+
+        plt.legend(bbox_to_anchor=(0.95, 0.1), loc='right', borderaxespad=0.)
+        plt.xlabel("Number of Images")
+        plt.ylabel("Total Time")
+        
+        cont += 1
+
+    #AVERAGE TIME
+    num_threads_x_avg_time = plt.figure(figsize=(16,16))
+    num_threads_x_avg_time.suptitle("Average Time vs Number of Images")
+
+    cont = 1
+    for i in datman.M_Thread_Array:
+        num_threads_x_avg_time.add_subplot(graph_rows,2, cont).title.set_text("Threads: " + str(i.id))
+        num_images = [cycle * int(i.id) for cycle in i.measurements[0].cycle]
+
+        for m in i.measurements:
+            avg_time = []
+            for x,times in enumerate(m.time):
+                avg_time.append(times/num_images[x])
+            
+            plt.plot(num_images, avg_time, label = name_changer(m.file_name),marker="o")
+
+        plt.legend(bbox_to_anchor=(0.95, 0.1), loc='right', borderaxespad=0.)
+        plt.xlabel("Number of Images")
+        plt.ylabel("Average Time")
+        cont += 1
+
     plt.show()
 
+
+def name_changer(name):
+    if name == "6785.bin":
+        return "Secuencial"
+    elif name == "6786.bin":
+        return "Heavy Process"
+    elif name == "6787.bin":
+        return "Pre-Heavy Process"
+    else:
+        return name
 
 if __name__ == "__main__":
     datman = Data_Manager()
@@ -24,3 +68,4 @@ if __name__ == "__main__":
     datman.get_data("6787.bin")
 
     graph_data()
+
